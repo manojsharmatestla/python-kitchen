1a52dbe17c2c090d18a2438175ec77c16f71c61c
Manoj Sharma <manoj_sharma@bmc.com>
2018-03-26 21:53:31 +0530

    Added the code to write to Yaml file

---
+++ yaml/Yaml_Writer.py
@@ -0,0 +1,11 @@
+import yaml
+data = dict(
+    A = 'a',
+    B = dict(
+        C = 'c',
+        D = 'd',
+        E = 'e',
+    )
+)
+with open('data.yml', 'w') as outfile:
+        yaml.dump(data, outfile)
